7059cb8fa1d5dd54c90bd174cec7368f90e882fe
Patrick Easters <patrick.easters@sysdig.com>
2024-02-09 14:02:51 -0500

    Use git repo for cs vpc

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "cs_vpc" {
-  # source = "git@github.com:draios/cs-terraform-modules.git//terraform-cs-aws-vpc"
-  source = "/Users/patrick.easters/git/draios/cs-terraform-modules/terraform-cs-aws-vpc"
+  source = "git@github.com:draios/cs-terraform-modules.git//terraform-cs-aws-vpc"
+  #source = "/Users/patrick.easters/git/draios/cs-terraform-modules/terraform-cs-aws-vpc"
   aws_region = var.aws_region
   nat_instance = true
 }
